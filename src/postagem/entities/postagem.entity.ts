69cb4a3473ea2ae1a8491247431e5cf1e5106b78
RaulVieira007 <raul.vieiras2001@gmail.com>
2025-08-06 17:36:58 -0300

    feat: adicionado entidade Usuario

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,32 +1,33 @@
-import { IsNotEmpty } from "class-validator";
-import { Tema } from "src/tema/entities/tema.entity";
-import {
-  Column,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { Tema } from "../../tema/entities/tema.entity"
+import { Usuario } from "../../usuario/entities/usuario.entity"
 
-@Entity({ name: "tb_postagens" }) // Crianção da tabela
+@Entity({name: "tb_postagens"})
 export class Postagem {
-  @PrimaryGeneratedColumn() // Chave primaria com prenchimento automatico.
-  id: number; // Coluna ID 
 
-  @IsNotEmpty()
-  @Column({ length: 100, nullable: false })
-  titulo: string; // Coluna Titulo 
+    @PrimaryGeneratedColumn()    
+    id: number
 
-  @IsNotEmpty()
-  @Column({ length: 1000, nullable: false })
-  texto: string; // Coluna texo
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    titulo: string
 
-  @UpdateDateColumn()
-  data: Date; // Coluna data
+    @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    texto: string
 
-  @ManyToOne(() => Tema, (tema) => tema.postagem, {
-    onDelete: "CASCADE"
-  })
-  tema : Tema;
-    static tema: any;
+    @UpdateDateColumn()
+    data: Date
+    
+    @ManyToOne(() => Tema, (tema) => tema.postagem, {
+        onDelete: "CASCADE"
+    })
+    tema: Tema
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE"
+    })
+    usuario: Usuario
+
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,39 +1,41 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'; // pacote Common com os respectivos decoradores
-import { DeleteResult, ILike, Repository } from 'typeorm'; // Repository do módulo typeorm
-import { Postagem } from '../entities/postagem.entity'; // Importamos a classe entidade Postagem, do módulo Postagem.
-import { InjectRepository } from '@nestjs/typeorm'; // pacote TypeORM com os respectivos decoradores
-import { TemaService } from 'src/tema/services/tema.service';
-import { Tema } from 'src/tema/entities/tema.entity';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { TemaService } from "../../tema/services/tema.service";
+import { Postagem } from "../entities/postagem.entity";
 
-@Injectable() // decorador @Injectable indica que a classe é um serviço, ou seja, uma classe que pode ser injetada em outras classes por meio da Injeção de Dependências.
+@Injectable()
 export class PostagemService {
   constructor(
-    @InjectRepository(Postagem) // tabela no banco de dados <---
+    @InjectRepository(Postagem)
     private postagemRepository: Repository<Postagem>,
-    private TemaService:TemaService
-  ) {}
+    private temaService: TemaService
+  ) { }
 
   async findAll(): Promise<Postagem[]> {
     return await this.postagemRepository.find({
       relations: {
-        tema: true
+        tema: true,
+        usuario: true
       }
-    }); // Equivalente a: SELECT * FROM tb_postagens;
+    });
   }
 
   async findById(id: number): Promise<Postagem> {
+
     const postagem = await this.postagemRepository.findOne({
       where: {
         id
       },
       relations: {
-        tema: true
+        tema: true,
+        usuario: true
       }
     });
 
     if (!postagem)
       throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
-      
+
     return postagem;
   }
 
@@ -43,27 +45,36 @@ export class PostagemService {
         titulo: ILike(`%${titulo}%`)
       },
       relations: {
-        tema: true
+        tema: true,
+        usuario: true
       }
-    });
+    })
   }
 
   async create(postagem: Postagem): Promise<Postagem> {
 
-    await this.findById(postagem.id)
-
-    await this.TemaService.findById(postagem.tema.id)
+    await this.temaService.findById(postagem.tema.id)
 
     return await this.postagemRepository.save(postagem);
+
   }
 
   async update(postagem: Postagem): Promise<Postagem> {
+
     await this.findById(postagem.id);
+
+    await this.temaService.findById(postagem.tema.id)
+
     return await this.postagemRepository.save(postagem);
+
   }
 
   async delete(id: number): Promise<DeleteResult> {
+
     await this.findById(id);
+
     return await this.postagemRepository.delete(id);
+
   }
+
 }
