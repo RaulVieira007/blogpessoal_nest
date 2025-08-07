5ce447f75ff25be26ee34b4cd8fe2e06854b8ca7
RaulVieira007 <raul.vieiras2001@gmail.com>
2025-08-07 18:56:48 -0300

    Criação da entidade auth

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,45 +1,47 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common"; // Importando o pacote Common com os respectivos decoradores, que serão utilizados na implementação da Classe PostagemController.
-import { PostagemService } from "../services/postagem.service"; // Importando a Classe PostagemService, que é responsável pela lógica de negócio relacionada à Postagem.
-import { Postagem } from "../entities/postagem.entity";  // Importando a Classe Postagem, do módulo Postagem.
-import { get } from "http";
+import { Controller, Get, Post, Put, Delete, HttpCode, HttpStatus, Param, Body, HttpException, UseGuards, ParseIntPipe } from "@nestjs/common";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
+import { Postagem } from "../entities/postagem.entity";
+import { PostagemService } from "../services/postagem.service";
 
-@Controller('/postagens') // @Controller indica que a classe é um RestController, Responsável por resqusição HTTP  (Exemplos: 200 para sucesso, 201 para criação, 404 para não encontrado).
-export class PostagemController{
-    constructor(private readonly postagemService: PostagemService){} // PostagemService dentro da controladora, permitindo que a lógica de negócios (como a recuperação, criação, atualização e exclusão de postagens) seja delegada ao serviço, mantendo a controladora focada apenas em gerenciar as requisições HTTP.
+@UseGuards(JwtAuthGuard)
+@Controller("/postagens")
+export class PostagemController {
+  constructor(private readonly postagemService: PostagemService) { }
 
-    @Get() // Rota
-    @HttpCode(HttpStatus.OK) //  decorador @HttpCode(HttpStatus.OK) é utilizado para definir o código de status HTTP que será retornado quando a requisição for processada com sucesso
-    findAll(): Promise<Postagem[]>{ // O método findAll() é um método assíncrono que retorna uma Promise
-        return this.postagemService.findAll(); // O método findAll() da PostagemService é chamado para realizar a busca de todas as postagens no banco de dados,
-    }
-        
-    @Get('/:id') // Rota
-    @HttpCode(HttpStatus.OK)
-    findByInd(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
-        return  this.postagemService.findById(id);
-    }
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Postagem[]> {
+    return this.postagemService.findAll();
+  }
 
-    @Get('/titulo/:titulo') // Rota
-    @HttpCode(HttpStatus.OK)
-    findByAllTitulo(@Param('titulo') titulo: string) : Promise<Postagem[]>{
-        return this.postagemService.findAllByTitulo(titulo);
-    }
-    
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() Postagem: Postagem): Promise<Postagem> {
-        return this.postagemService.create(Postagem);
-    }
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findById(id);
+  }
 
-    @Put()
-    @HttpCode(HttpStatus.OK)
-    update(@Body() postagem: Postagem): Promise<Postagem> {
-        return this.postagemService.update(postagem);
-    }
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.postagemService.findAllByTitulo(titulo);
+  }
 
-    @Delete('/:id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    delete(@Param('id', ParseIntPipe) id: number){
-        return this.postagemService.delete(id);
-    }
-}   
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.update(postagem);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number){
+    return this.postagemService.delete(id);
+  }
+
+}
